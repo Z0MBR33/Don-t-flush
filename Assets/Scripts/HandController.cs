0a85da6df0c8193cd30f15611ead32c94010dfef
Z0MBR33 <pascal.plewa97@hotmail.de>
2017-09-06 19:34:03 +0200

    BumperMechanic
    Bumper Script

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour {
+	public GameObject leftHand;
+    public GameObject rightHand;
+    public bool pressed;
+    public bool back;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space") && !pressed)
+        {
+            leftHand.GetComponent<HingeJoint2D>().useMotor = true;
+            rightHand.GetComponent<HingeJoint2D>().useMotor = true;
+            pressed = true;
+        }
+        
+        if (leftHand.GetComponent<HingeJoint2D>().jointAngle <= -70 && rightHand.GetComponent<HingeJoint2D>().jointAngle >= 70 && pressed)
+        {
+            leftHand.GetComponent<HingeJoint2D>().useMotor = false;
+            rightHand.GetComponent<HingeJoint2D>().useMotor = false;
+            back = true;
+            
+        }
+        if (leftHand.GetComponent<HingeJoint2D>().jointAngle >= -20 && rightHand.GetComponent<HingeJoint2D>().jointAngle <= 20 && back)
+        {
+            Debug.Log("hi");
+            pressed = false;
+            back = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public GameObject rootPoint;
+    private bool dirRight = true;
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        
+    }
+}
