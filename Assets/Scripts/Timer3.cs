51bdebef7f28c3797435d2fb05a76f9257f2e006
BrennendesEinhorn <selena.bobek@web.de>
2017-09-06 19:48:43 +0200

    timer verschoben

---
+++ Assets/Scripts/Timer3.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer3 : MonoBehaviour {
+
+    public Text timerText;
+    private float startTime;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+        float u = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+        
+
+
+        float countDown = 30 - t;
+        string countingDown = countDown.ToString("f2");
+
+
+        timerText.text = countingDown;
+
+
+	}
+}
