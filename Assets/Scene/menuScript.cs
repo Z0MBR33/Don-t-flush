0a1a4495f8cf9dbdddc5d6593cfc45894a4e8809
BrennendesEinhorn <selena.bobek@web.de>
2017-09-07 12:06:32 +0200

    Startmenü funktioniert

---
+++ Assets/Scene/menuScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour {
+
+    public Button startText;
+    public Button credits;
+    public Button leaveCredits;
+    public Canvas CreditCanvas;
+
+    void Start() {
+        startText = startText.GetComponent<Button>();
+        credits = credits.GetComponent<Button>();
+        leaveCredits = leaveCredits.GetComponent<Button>();
+
+
+        CreditCanvas = CreditCanvas.GetComponent<Canvas>();
+
+        CreditCanvas.enabled = true;
+        
+    }
+
+    public void StartLevel (){
+        SceneManager.LoadScene("test");
+        }
+
+
+    public void creditsPress()
+    {
+        CreditCanvas.enabled = false;
+    }
+
+    public void leaveCreditsPress()
+    {
+        SceneManager.LoadScene("startScreen");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
